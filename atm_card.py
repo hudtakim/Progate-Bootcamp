e9364881d863e9c14cdfa9f7e334ba54d689cd69
hudtakim <hudtakim@gmail.com>
2021-08-24 16:33:11 +0700

    Init ATMCard class

---
+++ atm_card.py
@@ -0,0 +1,15 @@
+from typing import DefaultDict
+
+
+class ATMCard():
+    def __init__(self, defaultPin, defaultBalance):
+        self.defaultPin = defaultPin
+        self.defaultBalance = defaultBalance
+
+    def info(self):
+        print("Default PIN:", self.defaultPin)
+        print("Default Balance:", self.defaultBalance)
+
+account1 = ATMCard(123, 5000)
+account1.info()
+
