893691bec8092be69dfcea6b30f5e534f023a78b
hudtakim <hudtakim@gmail.com>
2021-08-26 08:56:44 +0700

    Finishing basic features

--- atm_program.py
+++ atm_program.py
@@ -13,7 +13,7 @@ while(True):
 
     if userInput != ATM.get_custPin:
         print("Anda melakukan kesalahan 3 kali, PIN anda terblokir")
-        break
+        exit()
     
     print("Berhasil Masuk!!!")
 
@@ -22,22 +22,55 @@ while(True):
         print("\n--------Selamat Datang di Mesin ATM---------")
         print("1. Cek saldo")
         print("2. Debet")
-        print("3. Ganti PIN")
-        print("4. Keluar")
+        print("3. Deposit")
+        print("4. Ganti PIN")
+        print("5. Keluar")
 
         userInput = int(input("Pilih tindakan:"))
         if userInput == 1:
             print("Saldo anda: Rp.", ATM.get_custBalance)
-        elif userInput == 2:
-            print("Debet dipilih")
-        elif userInput == 3:
-            print("Ganti PIN dipilih")
-        elif userInput == 4:
-            print("Keluar dipilih")
-        else:
-            print("Input Invalid")
 
-        break
+        elif userInput == 2:
+            nominal = int(input("Masukkan nominal debet:"))
+            if ATM.custBalance > nominal:
+                ATM.withdrawBalance(nominal)
+                print("Sisa saldo: Rp.", ATM.get_custBalance)
+            else:
+                print("Saldo anda tidak mencukupi!")
+
+        elif userInput == 3:
+            nominal = int(input("Masukkan nominal deposit:"))
+            ATM.depositBalance(nominal)
+            print("Saldo anda menjadi: Rp.", ATM.get_custBalance)
+
+        elif userInput == 4:
+            verify = int(input("Masukkan PIN saat ini:"))
+            trial = 0
+            while verify != ATM.get_custPin and trial <2:
+                verify = int(input("PIN salah!, coba masukkan lagi:"))
+                trial += 1
+                
+            if verify != ATM.get_custPin:
+                print("Anda melakukan kesalahan 3 kali, PIN anda terblokir")
+                exit()
+
+            newPin = int(input("Masukkan PIN baru:"))
+            verify = int(input("Ulangi PIN baru:"))
+            while verify != newPin:
+                verify = int(input("PIN tidak cocok, ulangi PIN baru:"))
+            
+            print("PIN berhasil diganti")
+
+        elif userInput == 5:
+            print("--------Resi Transaksi-------")
+            print("No. Rekord: ", random.randint(100000, 1000000))
+            print("Tanggal: ", datetime.datetime.now())
+            print("Saldo akhir: ", ATM.get_custBalance)
+            print("Terima kasih telah menggunakan ATM Kami!")
+            exit()
+
+        else:
+            print("Input Invalid, silahkan pilih sesuai nomor menu!")
 
     break
     
