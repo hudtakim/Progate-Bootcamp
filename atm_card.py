5daa8a1685b92fd09988da1072a7ecc800a57995
hudtakim <hudtakim@gmail.com>
2021-08-26 08:19:28 +0700

    Membuat fitur cek saldo dan membuat property pada atm_card

--- atm_card.py
+++ atm_card.py
@@ -3,8 +3,10 @@ class ATMCard():
         self.defaultPin = defaultPin
         self.defaultBalance = defaultBalance
 
+    @property
     def get_defaultPin(self):
         return self.defaultPin
     
+    @property
     def get_defaultBalance(self):
         return self.defaultBalance
--- atm_program.py
+++ atm_program.py
@@ -1,3 +1,4 @@
+from atm_card import ATMCard
 from customer import Customer
 import random
 import datetime
@@ -9,10 +10,34 @@ while(True):
     while(trial < 2 and userInput != ATM.get_custPin):
         userInput = int(input("PIN salah, masukkan lagi:"))
         trial +=1
-    
-    if userInput == ATM.get_custPin:
-        print("Berhasil masuk")
-        break
-    else:
+
+    if userInput != ATM.get_custPin:
         print("Anda melakukan kesalahan 3 kali, PIN anda terblokir")
         break
+    
+    print("Berhasil Masuk!!!")
+
+    while(True):
+        
+        print("\n--------Selamat Datang di Mesin ATM---------")
+        print("1. Cek saldo")
+        print("2. Debet")
+        print("3. Ganti PIN")
+        print("4. Keluar")
+
+        userInput = int(input("Pilih tindakan:"))
+        if userInput == 1:
+            print("Saldo anda: Rp.", ATM.get_custBalance)
+        elif userInput == 2:
+            print("Debet dipilih")
+        elif userInput == 3:
+            print("Ganti PIN dipilih")
+        elif userInput == 4:
+            print("Keluar dipilih")
+        else:
+            print("Input Invalid")
+
+        break
+
+    break
+    
